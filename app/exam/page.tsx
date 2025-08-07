8211660101ee49667ffd04046089a88cc5698a1e
lymakara-dev <ly.makara.personal@gmail.com>
2025-08-07 14:24:34 +0700

    modify ci file

--- app/exam/page.tsx
+++ app/exam/page.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default function ExamPage
-() {
-  return (
-    <div>ExamPage</div>
-  )
+export default function ExamPage() {
+  return <div>ExamPage</div>;
 }
